ebdf3a6c5ede7dbc36fa007098305b77b70ba828
Vuqar Rahim <vuqarahim@gmail.com>
2021-05-26 18:20:53 +0400

    WebApi implementation CarsController Setup

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -59,7 +59,7 @@ namespace Business.Concrete
 
         public IDataResult<Car> GetById(int id)
         {
-            throw new NotImplementedException();
+            return new SuccessDataResult<Car>(_carDal.Get(c => c.Id == id));
         }
 
         public IDataResult<List<CarDetailDto>> GetCarDetails()
